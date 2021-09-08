bc5f06d5f36b9d8f1e97f0d581293a0e373edb5d
cicerogusta <ciceroandrade2525@gmail.com>
2021-09-08 12:08:59 -0300

    ajustes endpoint autenticacao token

--- src/main/java/com/ciceropinheiro/conductor/Spring/jwt/JwtController.java
+++ src/main/java/com/ciceropinheiro/conductor/Spring/jwt/JwtController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/authenticate")
-@Api(tags = "Jwt")
 public class JwtController {
 
     @Autowired
