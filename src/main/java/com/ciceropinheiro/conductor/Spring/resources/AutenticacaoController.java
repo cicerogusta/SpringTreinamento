59955f696c6ca7e57cf81a7d09ac347e8960890e
cicerogusta <ciceroandrade2525@gmail.com>
2021-09-16 09:45:14 -0300

    ajustes swagger autenticacao

--- src/main/java/com/ciceropinheiro/conductor/Spring/resources/AutenticacaoController.java
+++ src/main/java/com/ciceropinheiro/conductor/Spring/resources/AutenticacaoController.java
@@ -3,8 +3,9 @@ package com.ciceropinheiro.conductor.Spring.resources;
 import com.ciceropinheiro.conductor.Spring.config.security.TokenService;
 import com.ciceropinheiro.conductor.Spring.dto.request.LoginRequest;
 import com.ciceropinheiro.conductor.Spring.dto.request.TokenRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,15 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
+@Api(tags = "Autenticacao")
 @RequestMapping("/auth")
 public class AutenticacaoController {
 
     @Autowired
     private AuthenticationManager authManager;
 
-    private TokenService tokenService;
+    private final TokenService tokenService;
+
+    public AutenticacaoController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
 
     @PostMapping
+    @ApiOperation(value = "Autentica Cliente")
+
     public ResponseEntity<TokenRequest> autenticar(@RequestBody @Valid LoginRequest loginRequest) {
         UsernamePasswordAuthenticationToken dadosLogin = loginRequest.converter();
 
