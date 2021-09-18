e6fd0cf26d47934fc97e86afc7bc095d1975f114
cicerogusta <ciceroandrade2525@gmail.com>
2021-09-17 23:58:44 -0300

    ApiVendas1.0 ok

--- src/main/java/com/ciceropinheiro/conductor/Spring/dto/request/TokenRequest.java
+++ src/main/java/com/ciceropinheiro/conductor/Spring/dto/request/TokenRequest.java
@@ -1,5 +1,10 @@
 package com.ciceropinheiro.conductor.Spring.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TokenRequest {
     private String token;
     private String tipo;
--- src/main/java/com/ciceropinheiro/conductor/Spring/resources/AutenticacaoController.java
+++ src/main/java/com/ciceropinheiro/conductor/Spring/resources/AutenticacaoController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,7 @@ public class AutenticacaoController {
     }
 
     @PostMapping
-    public ResponseEntity<TokenRequest> autenticar(@RequestBody  LoginRequest loginRequest) {
+    public ResponseEntity<TokenRequest> autenticar(@RequestBody @Validated LoginRequest loginRequest) {
         UsernamePasswordAuthenticationToken dadosLogin = loginRequest.converter();
 
         try {
